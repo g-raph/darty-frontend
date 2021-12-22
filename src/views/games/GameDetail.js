70a96f1b0129dfd644156e05bb9e5d942feebdd2
Gielen Gunter <Gunter.Gielen@cegeka.com>
2021-12-22 15:23:09 +0100

    nice one

--- src/views/games/GameDetail.js
+++ src/views/games/GameDetail.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import { Card, CardBody, CardHeader, CardTitle, Col, Row, Table } from 'reactstrap';
 import ScoreAddForm from './ScoreAddForm';
 import { getGame } from '../../redux/actions/gamesActions';
+import { Line } from 'react-chartjs-2';
 // const nextPlayerIndex = 0;
 
 class GameDetail extends Component {
@@ -43,23 +44,45 @@ class GameDetail extends Component {
   
     }
 
-    const arrangeNextPlayer = () => {
-      let playerIndex = this.state.currPlayer;
-      if (playerIndex < (game.players.length - 1)) {
-        playerIndex = playerIndex + 1;
-      } else {
-        playerIndex = 0;
-      }
-      this.setState({currPlayer: playerIndex});
-    }
-
     const setScoreAndSetNextPlayer = (score) => {
       const pageId = window.location.pathname.slice(12);
       this.props.getGame(pageId);
       arrangeScores();
-      arrangeNextPlayer();
       console.log('STATE', this.state);
     }
+
+    const playerStats = {
+      data: (canvas) => {
+        const datasetsArray = [];
+        this.state.scoreArray.forEach(player => {
+          console.log(player);
+          const randomColor = '#' + Math.floor(Math.random()*16777215).toString(16);
+          const datasetObject = {
+            data: player.worps.length > 0 ? player.worps.map(item => item.arrowTotal) : 0,
+            fill: true,
+            borderColor: randomColor,
+            backgroundColor: "transparent",
+            pointBorderColor: randomColor,
+            pointRadius: 4,
+            pointHoverRadius: 4,
+            pointBorderWidth: 8,
+            tension: 0.4,
+            label: player.Name
+          }
+          datasetsArray.push(datasetObject);
+        });
+        console.log('datasetsArray', datasetsArray);
+        return {
+          labels: this.state.scoreArray[0].worps.map((item, index) => index + 1),
+          datasets: datasetsArray,
+        };
+      },
+      options: {
+        plugins: {
+          legend: { display: true },
+        },
+      },
+    };
 
     if (!game || game.length === 0) {
       return <p>No game, sorry</p>;
@@ -75,7 +98,7 @@ class GameDetail extends Component {
       <Card>
         <CardHeader>
           <CardTitle tag="h4">Game {new Date(game.name).toLocaleString()}</CardTitle>
-          <ScoreAddForm currentPlayerIndex={this.state.currPlayer} game={game} setScore={(score) => setScoreAndSetNextPlayer(score)} />
+          <ScoreAddForm parentState={this.state.scoreArray} game={game} setScore={(score) => setScoreAndSetNextPlayer(score)} />
         </CardHeader>
         <CardBody>
           <div className="game-detail--left">bord hier</div>
@@ -112,6 +135,24 @@ class GameDetail extends Component {
       </Card>
       </Col>
         </Row>
+        <Row>
+            <Col md="12">
+            <Card className="card-chart">
+              <CardHeader>
+                <CardTitle tag="h5">Worp statistieken</CardTitle>
+                <p className="card-category">Een overzicht van alle worpen</p>
+              </CardHeader>
+              <CardBody>
+                <Line
+                  data={playerStats.data}
+                  options={playerStats.options}
+                  width={400}
+                  height={150}
+                />
+              </CardBody>
+            </Card>
+            </Col>
+          </Row>
       </div>
     </>
     )
--- src/views/games/ScoreAddForm.js
+++ src/views/games/ScoreAddForm.js
@@ -1,13 +1,17 @@
 import axios from 'axios';
 import React, { useState } from 'react';
+import NotificationAlert from "react-notification-alert";
 
 function ScoreAddForm(props) {
-  const {game} = props;
+  const notificationAlert = React.useRef();
+  const {game, parentState} = props;
   const [form, setState] = useState({
     pijl1: 0,
     pijl2: 0,
     pijl3: 0,
   });
+
+  console.log('parentState', parentState);
 
   const handleChange = (evt) => {
     const value = evt.target.value;
@@ -17,57 +21,80 @@ function ScoreAddForm(props) {
     });
   };
 
+  const notify = (place, nextPlayer) => {
+    let options = {};
+    options = {
+      place: place,
+      message: (
+        <div>
+          <p>
+            Volgende speler: <strong>{nextPlayer}</strong>
+          </p>
+        </div>
+      ),
+      type: 'success',
+      icon: "nc-icon nc-bell-55",
+      autoDismiss: 7,
+    };
+    notificationAlert.current.notificationAlert(options);
+  };
+
   const handleSubmit = (evt) => {
     evt.preventDefault();
-    const prevScore = 
-    game.players[game.currentPlayerIndex].worps ? 
-    game.players[game.currentPlayerIndex].worps.slice(-1)[0].newScore : 
-    game.startScore;
-    const totalScore = parseInt(form.pijl1) + parseInt(form.pijl2) + parseInt(form.pijl3);
-    const nextScore = prevScore - totalScore;
-    const scoreObj = {
-      previousScore: prevScore,
-      arrow1: form.pijl1,
-      arrow2: form.pijl2,
-      arrow3: form.pijl3,
-      arrowTotal: totalScore,
-      newScore: nextScore >= 0 ? nextScore : prevScore,
-      player: game.players[game.currentPlayerIndex],
-      game: game
-    };
-    axios
-    .post('http://localhost:1337/worps', scoreObj)
-    .then(response => response.data)
-    .then(data => {
-      props.setScore(data);
-    });  
-    const nextPlayerObject = {
-      currentPlayerIndex: (game.currentPlayerIndex < (game.players.length - 1)) ? game.currentPlayerIndex + 1 : 0
-    };
-    axios
+    if (game) {
+      const prevScore = parentState[game.currentPlayerIndex].worps.length > 0 ? 
+                          parentState[game.currentPlayerIndex].worps[parentState[game.currentPlayerIndex].worps.length - 1].newScore : 
+                          game.startScore;
+      const totalScore = parseInt(form.pijl1) + parseInt(form.pijl2) + parseInt(form.pijl3);
+      const nextScore = prevScore - totalScore;
+      const scoreObj = {
+        previousScore: prevScore,
+        arrow1: form.pijl1,
+        arrow2: form.pijl2,
+        arrow3: form.pijl3,
+        arrowTotal: totalScore,
+        newScore: nextScore >= 0 ? nextScore : prevScore,
+        player: parentState[game.currentPlayerIndex],
+        game: game
+      };
+      axios
+      .post('http://localhost:1337/worps', scoreObj)
+      .then(response => response.data)
+      .then(data => {
+        props.setScore(data);
+      });
+      const nextPlayerObject = {
+        currentPlayerIndex: (game.currentPlayerIndex < (game.players.length - 1)) ? game.currentPlayerIndex + 1 : 0
+      };
+      axios
       .put('http://localhost:1337/games/' + game.id, nextPlayerObject)
       .then(response => response.data)
       .then(data => {
         props.setScore(data);
-      });  
+        notify('tc', game.players[nextPlayerObject.currentPlayerIndex].Name);
+        });  
+    }
   }
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        Pijl 1: 
-        <input type="number" name="pijl1" value={form.pijl1} onChange={handleChange} />
-      </label>
-      <label>
-        Pijl 2: 
-        <input type="number" name="pijl2" value={form.pijl2} onChange={handleChange} />
-      </label>
-      <label>
-        Pijl 3: 
-        <input type="number" name="pijl3" value={form.pijl3} onChange={handleChange} />
-      </label>
-      <input type="submit" value="Score toevoegen" />
-    </form>
+    <div className='scoreform'>
+      <NotificationAlert ref={notificationAlert} />
+      <form onSubmit={handleSubmit}>
+        <label>
+          Pijl 1: 
+          <input type="number" name="pijl1" value={form.pijl1} onChange={handleChange} />
+        </label>
+        <label>
+          Pijl 2: 
+          <input type="number" name="pijl2" value={form.pijl2} onChange={handleChange} />
+        </label>
+        <label>
+          Pijl 3: 
+          <input type="number" name="pijl3" value={form.pijl3} onChange={handleChange} />
+        </label>
+        <input type="submit" value="Score toevoegen" />
+      </form>
+    </div>
   );
 };
 export default ScoreAddForm;
